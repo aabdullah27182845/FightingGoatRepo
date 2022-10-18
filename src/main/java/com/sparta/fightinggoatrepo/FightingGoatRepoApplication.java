184b782ed59df58ba15c1c8b78c95ed5cb8f9c64
Abdullah Navid <abdullahnavid5@gmail.com>
2022-10-18 14:03:16 +0100

    --

---
+++ src/main/java/com/sparta/fightinggoatrepo/FightingGoatRepoApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.fightinggoatrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FightingGoatRepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FightingGoatRepoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sparta/fightinggoatrepo/FightingGoatRepoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.fightinggoatrepo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FightingGoatRepoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
